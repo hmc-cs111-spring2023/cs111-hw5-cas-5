46866169fc26ce3a6e7ca484acd756eef86780a8
Cas Ruedy <89892181+Cruedy@users.noreply.github.com>
2023-02-20 21:33:46 +0000

    routine

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -1,3 +1,16 @@
 package dfa // leave this line in the file
 
-// TODO: replace this comment with your implementation
+case class State(label: String)
+
+case class Transition(from: State, symbol: Char, to: State)
+
+class DFA(states: Set[State], transitions: Set[Transition], startState: State, acceptingStates: Set[State]) {
+    def accepts(states: String): Boolean = 
+        val states_state = State(states)
+        if (acceptingStates.contains(states_state)) {
+            true
+        }
+        else {
+            false
+        } 
+}
